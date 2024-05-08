2b9192cc7d4ab9bf8c4e82a1d47b65eddb7234d5
iramaroliveira <iramar.oliveira@trc.sebraeba.com.br>
2024-05-08 15:31:54 -0300

    Criado menu e fragments do sistema

--- app/src/main/java/br/com/cairu/projeto/integrador/brecho/MainActivity.java
+++ app/src/main/java/br/com/cairu/projeto/integrador/brecho/MainActivity.java
@@ -7,6 +7,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +23,14 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        loadFragment(new CatalogFragment());
+    }
+
+    private void loadFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
     }
 }
